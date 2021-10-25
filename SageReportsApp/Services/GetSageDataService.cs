af64f9c5bed24e6d8fd2983da9f4666edcd6054c
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2021-10-25 17:36:42 +0200

    Sage data services

--- SageReportsApp/Services/GetSageDataService.cs
+++ SageReportsApp/Services/GetSageDataService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SageReportsApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace SageReportsApp.Services
 {
-    class GetSageDataService
+    public class GetSageDataService
     {
+        public string _connectionString { get; protected set; }
+        public GetSageDataService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<SageVatRegister> GetVatRegisterData(int registerType)
+        {
+            var registerData = new List<SageVatRegister>();
+            return registerData;
+        }
     }
 }
