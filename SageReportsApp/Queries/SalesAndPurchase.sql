dc4c3b7246cbc5630c627f53b19f909c285f1698
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2021-10-25 15:45:16 +0200

    DB query

---
+++ SageReportsApp/Queries/SalesAndPurchase.sql
@@ -0,0 +1,22 @@
+﻿SELECT
+    d.[zrodlo],
+    r.[id],r.[def],FK.defRej.rNazwa,d.[skrot],d.[numer],r.[dId],r.[rokDok],r.[numerWl],r.[datadok],r.[dataWS],
+	r.[kontrahent],
+	'nip' = ISNULL( dkh.[nip], '' ),
+	r.[typabc],r.[okres],r.[nienaliczany],
+	[netto],[vat],r.[brutto], r.[stawka],
+	d.[sygnatura],d.[tresc], d.[nazwa], d.[dataOkr], r.[Oczek], d.[dataOper],
+	r.[znacznik],
+	'kh_nazwa' = ISNULL( dkh.[nazwa], '' ),
+	r.[ue], r.[usluga], r.[sOczek]
+	,r.[bruttoWaluta], r.[nettoWaluta], r.[vatWaluta]
+FROM FK.[rejVat] AS r  JOIN FK.[dokumenty] AS d
+ON  r.[dId]=d.[id] AND d.[rokId] = r.[rokDok]
+JOIN FK.FROK ON FK.FROK.rokId = d.rokId
+JOIN FK.defRej ON FK.defRej.id = r.def
+LEFT JOIN FK.danekh dkh ON dkh.id = r.danekh
+WHERE ( (r.[def] = -2 AND 2=0) OR defRej.rZs = 2) AND (1 <> 0 or r.[netto] <> 0) -- defRej.rZs = 1 zakup / defRej.rZs = 2 sprzedaż
+	AND d.dataOper between '2020-01-01' and '2020-12-31'  --( ( r.okres >= FROK.poczatek AND r.okres <= FROK.koniec) OR (d.[rokId] = 19))
+	and r.kontrahent in (2148,1914,9142,17275,1385,10711,10396,9166,17274,9373,10022,12801,12800,12961,16012,15435,15314,1365,17167,1013,17097,17320,42993,42992,47305,47235,14422,14423,14832,15117,38038,56627,9153,89427) 
+order by
+d.datadok
