dfb7c635237304c027155a716584893073ac4849
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2021-10-25 17:34:27 +0200

    Code cleanup

--- SageReportsApp/Data/AppDbContext.cs
+++ SageReportsApp/Data/AppDbContext.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SageReportsApp.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SageReportsApp.Data
 {
