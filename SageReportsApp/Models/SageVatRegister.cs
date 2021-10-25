976563c4f20b3088df769f8a53818f4b8833e923
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2021-10-25 17:39:40 +0200

    Sage services and models

--- SageReportsApp/Models/SageVatRegister.cs
+++ SageReportsApp/Models/SageVatRegister.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SageReportsApp.Models
+﻿namespace SageReportsApp.Models
 {
     public class SageVatRegister
     {
